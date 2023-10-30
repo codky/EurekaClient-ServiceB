75ea8f19047e7a37da21c9244c32d6cae3e8204f
codky <msky0912@gmail.com>
2023-10-30 15:47:22 +0900

    Initial commit

---
+++ src/test/java/com/example/secondservicepractice/SecondServicePracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.secondservicepractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondServicePracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
